fdfc43153b4ba1965b1b0fb8a37da8a27ac27957
Scott Julian <scottjulian7@gmail.com>
2015-08-13 10:15:17 -0600

    working!

--- ios/VPAID-Demo/VPAID-Demo/ViewController.swift
+++ ios/VPAID-Demo/VPAID-Demo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import SWXMLHash
 
-let vastURL = "http://search.spotxchange.com/vast/2.00/85394?VPAID=1prefetch=false&autoinit=false&autoplay=true&cb=1438140245814&mutable=0&app%5Bsdkversion%5D=2.1%20(2015072821)&device%5Bdpidsha1%5D=79aae48c-19c6-4ca0-a4a1-adbf160d9684&app%5Bname%5D=SpotX%20SDK%20Demo&app%5Bversion%5D=2.1&app%5Bbundle%5D=com.spotxchange.demo&app%5Bdomain%5D=com.spotxchange.demo&device%5Bconnectiontype%5D=2&device%5Bos%5D=Android&device%5Bosv%5D=5.0.2&device%5Bmake%5D=samsung&device%5Bmodel%5D=SM-G920T&device%5Bdevicetype%5D=1&device%5Bgeo%5D%5Blat%5D=0.0&device%5Bgeo%5D%5Blong%5D=0.0&device%5Bgeo%5D%5Bcountry%5D=USA&device%5Bip%5D=fe80%3A%3Ae850%3A8bff%3Afe32%3A43fb%25p2p0&device%5Bcarrier%5D=310260"
+let vastURL = "http://search.spotxchange.com/vast/2.00/85394?VPAID=js&prefetch=0&autoinit=1&autoplay=1&cb=1438140245814&mutable=0&app%5Bsdkversion%5D=2.1%20(2015072821)&device%5Bdpidsha1%5D=79aae48c-19c6-4ca0-a4a1-adbf160d9684&app%5Bname%5D=SpotX%20SDK%20Demo&app%5Bversion%5D=2.1&app%5Bbundle%5D=com.spotxchange.demo&app%5Bdomain%5D=com.spotxchange.demo&device%5Bconnectiontype%5D=2&device%5Bos%5D=Android&device%5Bosv%5D=5.0.2&device%5Bmake%5D=samsung&device%5Bmodel%5D=SM-G920T&device%5Bdevicetype%5D=1&device%5Bgeo%5D%5Blat%5D=0.0&device%5Bgeo%5D%5Blong%5D=0.0&device%5Bgeo%5D%5Bcountry%5D=USA&device%5Bip%5D=fe80%3A%3Ae850%3A8bff%3Afe32%3A43fb%25p2p0&device%5Bcarrier%5D=310260"
 
 let adBrokerUrl = "http://aka.spotxcdn.com/media/videos/js/ad/InstreamAdBroker_2015-07-07_14-37.debug.js";
 //let adBrokerUrl = "http://deals.lod.search.spotxchange.com/ad_player/listing_type/5.js"
@@ -49,9 +49,32 @@ class ViewController: UIViewController {
 
     func loadTheWebView(adParams: String, mediaUrl: String){
         var html = self.createHtml(adParams, mediaUrl: mediaUrl)
-        println(html)
+        //println(html)
         vpaidWebView.loadHTMLString(createHtml(adParams, mediaUrl: mediaUrl), baseURL: nil)
         self.view.addSubview(vpaidWebView)
+
+        let delay = 2 * Double(NSEC_PER_SEC)
+        let delay2 = 4 * Double(NSEC_PER_SEC)
+        var time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
+        dispatch_after(time, dispatch_get_main_queue()){
+            self.initAd();
+        }
+
+        time = dispatch_time(DISPATCH_TIME_NOW, Int64(delay2))
+        dispatch_after(time, dispatch_get_main_queue()){
+            self.startAd();
+        }
+
+    }
+
+    func initAd(){
+        println("INITAD")
+        vpaidWebView.stringByEvaluatingJavaScriptFromString("window.oAdOS.initAd(iContentWidth, iContentHeight, oEnvVars.media_transcoding, 0, JSON.stringify(oCreativeData), oEnvVars);");
+    }
+
+    func startAd(){
+        println("STARTAD")
+        vpaidWebView.stringByEvaluatingJavaScriptFromString("window.oAdOS.startAd();");
     }
 
     func createHtml(adParams: String, mediaUrl: String) -> String{
@@ -62,11 +85,9 @@ class ViewController: UIViewController {
                 "var oAdOS, iContentWidth = window.innerWidth || 320, iContentHeight = window.innerHeight || 240, strViewMode = \"normal\", oEnvVars = {}; \n" +
                 "var oCreativeData = %@; \n" +
                 "if(document.readyState == \"complete\"){ window.oAdOS = getVPAIDAd(); } else { window.onload = function() { window.oAdOS = getVPAIDAd(); }}; \n" +
-                "window.oAdOS.initAd(iContentWidth, iContentHeight, oEnvVars.media_transcoding, 0, JSON.stringify(oCreativeData), oEnvVars); \n" +
-                "window.oAdOS.startAd(); \n" +
             "</script>" +
             "</head> " +
-            "<body></body></html>", adBrokerUrl, adParams)
+            "<body></body></html>", mediaUrl, adParams)
     }
 
 }
