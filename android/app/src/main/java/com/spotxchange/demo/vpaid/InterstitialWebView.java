2f08e7b8543ebe8dac9662dc4a08c1cde3f755cc
Patrick <pmitchell@spotxchange.com>
2015-08-17 17:19:20 -0600

    VPAID Demo with minimal VAST parser

--- android/app/src/main/java/com/spotxchange/demo/vpaid/InterstitialWebView.java
+++
@@ -1,4 +0,0 @@
-package com.spotxchange.demo.vpaid;
-
-import android.content.Context;
-import android.webkit.WebView;
--- android/app/src/main/java/com/spotxchange/demo/vpaid/MainActivity.java
+++ android/app/src/main/java/com/spotxchange/demo/vpaid/MainActivity.java
@@ -2,22 +2,33 @@ package com.spotxchange.demo.vpaid;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.DownloadListener;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.spotxchange.demo.vpaid.vastparser.VASTParser;
+import com.spotxchange.demo.vpaid.vastparser.VPAIDResponse;
+
+import org.w3c.dom.Text;
+import org.xml.sax.InputSource;
+
+import java.io.StringReader;
 
 
 public class MainActivity extends Activity {
-    private static final String TOAST_TEXT = "Loading channel 68801...";
-
+    private Button _loadButton;
     private Button _showButton;
     private WebView _adWebView;
 
@@ -25,6 +36,15 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        _loadButton = ((Button) findViewById(R.id.load_button));
+        _loadButton.setEnabled(true);
+        _loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                loadVASTResponse();
+            }
+        });
 
         // Create the next level button, which tries to show an interstitial when clicked.
         _showButton = ((Button) findViewById(R.id.show_button));
@@ -38,10 +58,57 @@ public class MainActivity extends Activity {
 
         // Create the InterstitialAd and set the adUnitId (defined in values/strings.xml).
         _adWebView = ((WebView) findViewById(R.id.interstitial));
-        loadInterstitial();
 
         // Toasts the test ad message on the screen. Remove this after defining your own ad unit ID.
-        Toast.makeText(this, TOAST_TEXT, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Ready to load ads.", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadVASTResponse() {
+        TextView target = ((TextView) findViewById(R.id.target));
+        String targetUrl =  target.getText().toString();
+
+        Toast.makeText(this, "Loading " + targetUrl, Toast.LENGTH_SHORT).show();
+
+        VASTDownloadTask.VASTResponseHandler handler = new VASTDownloadTask.VASTResponseHandler() {
+            @Override
+            public void onSuccessResponse(String response) {
+                Log.d("VASTResponseHandler", response);
+                final VPAIDResponse vpaidResponse = VASTParser.read(new InputSource(new StringReader(response)));
+                Log.d("VASTResponseHandler", vpaidResponse.mediaUrl);
+                Log.d("VASTResponseHandler", vpaidResponse.adParameters);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        loadInterstitial(vpaidResponse);
+                    }
+                });
+            }
+
+            @Override
+            public void onEmptyResponse() {
+                Log.d("VASTResponseHandler", "Received empty VAST response.");
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Received empty response when loading VAST.", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onFailure(final String response) {
+                Log.e("VASTResponseHandler", "Failure: " + response);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(MainActivity.this, "Received failure message when loading VAST: " + response, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        };
+
+        new VASTDownloadTask(handler).execute(targetUrl);
     }
 
 
@@ -66,10 +133,16 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    @TargetApi(Build.VERSION_CODES.KITKAT)
     private void showInterstitial() {
         // Show the ad if it's ready. Otherwise toast and reload the ad.
         if(_adWebView != null) {
             _adWebView.setVisibility(View.VISIBLE);
+            _adWebView.evaluateJavascript(
+                    getString(R.string.jsStartAd),
+                    null
+            );
+            Toast.makeText(MainActivity.this, "start", Toast.LENGTH_SHORT).show();
         }
         else {
             Toast.makeText(this, "Ad did not load", Toast.LENGTH_SHORT).show();
@@ -78,67 +151,87 @@ public class MainActivity extends Activity {
     }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
-    private void loadInterstitial() {
+    private void loadInterstitial(final VPAIDResponse vpaidResponse) {
         _showButton.setEnabled(false);
 
         WebView.setWebContentsDebuggingEnabled(true);
         _adWebView.setVisibility(View.INVISIBLE);
         _adWebView.getSettings().setJavaScriptEnabled(true);
-        //_adWebView.loadUrl("http://local.spotxcdn.com/adcallback");
+        _adWebView.addJavascriptInterface(new VpaidAdInterface(this, vpaidResponse), "NativeInterface");
+
         _adWebView.loadDataWithBaseURL(
                 "http://search.spotxchange.com",
-                getString(R.string.PLACEHOLDER_htmlAdBrokerScript),
+                String.format(
+                        getString(R.string.htmlAdBrokerScript),
+                        vpaidResponse.mediaUrl
+                ),
                 "text/html",
                 "utf8",
                 null
         );
 
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+        Toast.makeText(MainActivity.this, "Constructing VPAID ad...", Toast.LENGTH_SHORT).show();
+    }
+
+    private void evaluateJavascript(final String javascript)
+    {
+        runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                _showButton.setEnabled(true);
-
-                _adWebView.evaluateJavascript(
-                        getString(R.string.jsEnvironment),
-                        null
-                );
-                _adWebView.evaluateJavascript(
-                        getString(R.string.PLACEHOLDER_jsAdParameters),
-                        null
-                );
-
-                _adWebView.evaluateJavascript(
-                        getString(R.string.jsGetVPAIDAd),
-                        null
-                );
-                Toast.makeText(MainActivity.this, "ctor", Toast.LENGTH_SHORT).show();
+                _adWebView.evaluateJavascript(javascript, null);
             }
-        }, 5000);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                _adWebView.evaluateJavascript(
-                    getString(R.string.jsInitAd),
-                    null
-                );
-                Toast.makeText(MainActivity.this, "init", Toast.LENGTH_SHORT).show();
-            }
-        }, 7000);
-
-        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                _adWebView.evaluateJavascript(
-                    getString(R.string.jsStartAd),
-                    null
-                );
-                Toast.makeText(MainActivity.this, "start", Toast.LENGTH_SHORT).show();
-            }
-        }, 10000);
+        });
     }
 
     private void closeAd() {
-        loadInterstitial();
+        //loadInterstitial();
+    }
+
+    private class VpaidAdInterface {
+        Context _context;
+        VPAIDResponse _response;
+
+        VpaidAdInterface(Context context, VPAIDResponse vpaidResponse)
+        {
+            _context = context;
+            _response = vpaidResponse;
+        }
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @JavascriptInterface
+        public void onPageLoaded() {
+            evaluateJavascript(
+                getString(R.string.jsEnvironment)
+            );
+
+            evaluateJavascript(
+                String.format(
+                    getString(R.string.jsAdParameters),
+                    _response.adParameters
+                )
+            );
+
+            evaluateJavascript(
+                    getString(R.string.jsGetVPAIDAd)
+            );
+        }
+
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @JavascriptInterface
+        public void onGetVpaidAd() {
+            evaluateJavascript(
+                    getString(R.string.jsInitAd)
+            );
+        }
+
+        @JavascriptInterface
+        public void onAdLoaded() {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    _showButton.setEnabled(true);
+                }
+            });
+        }
     }
 }
